e0e49de3cd8817247c499b38c6bffcae17665b98
thismarcoantonio <thismarcoantonio@gmail.com>
2018-10-21 16:10:43 -0300

    [upd] updating port to dynamic;

--- server/src/index.js
+++ server/src/index.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('hello world'))
 
-app.listen(9000, () => console.log('running at port 9000'))
+app.listen(process.env.PORT || 3000, () => console.log('running at port 9000'))
