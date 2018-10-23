5b3caf5b05e136d29689b3cdd5707762cf78cb39
thismarcoantonio <thismarcoantonio@gmail.com>
2018-10-22 23:41:59 -0300

    [upd] adding sqlite instead postgreSql;

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,6 @@
 import express from 'express'
-import db from './models'
 const app = express()
 
 app.get('/', (req, res) => res.send('hello world'))
 
-app.listen(process.env.PORT || 3000, () => {
-  db.sequelize
-    .authenticate()
-    .then(() => console.log(`app running on port ${process.env.PORT || 3000}`))
-    .catch(err => console.log(`error on db: ${err}`))
-})
+app.listen(process.env.PORT || 3000)
